c752a54eb9122a871e04f580d08647d674286cec
SCOTT GUY (1516250) <1516250@rgu.ac.uk>
2018-03-01 09:36:38 +0000

    ok

---
+++ LAb9/contentList.js
@@ -0,0 +1,31 @@
+$(function(){
+  $("#addcontent").click(function(){
+    $("#")
+
+
+
+
+	// complete the following using jQuery
+
+	// 1. find the addcontent button in task-two.html
+	// 2. add an event listener for 'click' events
+	// 3. assign the addContent() function to the click event
+});
+
+function addContent () {
+	// add a list of items to the content div
+	var items = ["hewey", "dewey", "louie"];
+
+	// build the html string for a <ul> list
+	var items_html = "<ul>";
+	for (var i=0; i < items.length; i++) {
+		item = items[i];
+		items_html += "<li>" + item + "</li>";
+	};
+	items_html += "</ul>";
+
+	// using jQuery
+	// 1. find the content div
+	// 2. modify its html attribute by adding items_html
+
+}
